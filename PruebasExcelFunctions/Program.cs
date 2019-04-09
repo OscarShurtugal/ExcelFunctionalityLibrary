409f109675d82a1d36efafaf443306f17ed108b9
Oscar Shurtugal <oass1_power@hotmail.com>
2019-04-09 17:08:54 -0500

    Respaldo

--- PruebasExcelFunctions/Program.cs
+++ PruebasExcelFunctions/Program.cs
@@ -18,7 +18,7 @@ namespace PruebasExcelFunctions
         static void Main(string[] args)
         {
 
-            string pathToExcel = @"C:\Users\oscarsanchez2\Desktop\Input.xlsx";
+            string pathToExcel = @"C:\Users\oscarsanchez2\Desktop\210119_RB11P5MAYO.xlsx";
 
             Microsoft.Office.Interop.Excel.Application myExcel = new Application();
 
@@ -35,11 +35,57 @@ namespace PruebasExcelFunctions
             //1033: en-us
             //2058: es-mx
 
-            Console.WriteLine(getExcelSheetNames(pathToExcel));
+            Console.WriteLine(EraseExcelCells(pathToExcel));
 
             Console.ReadLine();
 
         }
+
+
+        public static string vLookUpEmulator(string pathToExcelFile)
+        {
+
+
+            //I create an instance of a Microsoft Excel Application
+            Microsoft.Office.Interop.Excel.Application myExcel = new Microsoft.Office.Interop.Excel.Application();
+            //Set visible to false to let the app work "behind".. a.k.a not OPEN the excel application
+            myExcel.Visible = false;
+            //Using the excel application instance, I open the book with the path to my excel file as parameter
+            Microsoft.Office.Interop.Excel.Workbook workbook = myExcel.Workbooks.Open(pathToExcelFile);
+            //Later I use a Worksheet Instance to get the actual sheet in the Excel File
+            Worksheet worksheet = myExcel.ActiveSheet;
+
+            Microsoft.Office.Interop.Excel.Range range = (Microsoft.Office.Interop.Excel.Range)myExcel.get_Range(worksheet.Cells[1, 1], worksheet.Cells[99, 99]);
+
+            string val = myExcel.WorksheetFunction.VLookup("Valor", range, 1,false);
+
+            //This string will hold all the sheet's names
+            string excelSheet = "";
+
+            //In this loop I concatenate the worksheet names into the string
+            foreach (Worksheet hoja in myExcel.Worksheets)
+            {
+                excelSheet += hoja.Name.ToString();
+                //This is the separator I chose to delimitate the sheet names
+                excelSheet += "¬";
+            }
+
+            //We close our workbook at the end of our process
+            workbook.Close();
+
+            //As a sideNote, Excel InteropServices don't release the object in memory, there's an instance of Excel still running
+            //We use Marchal Final Release to release the object and close the Excel Process
+            Marshal.FinalReleaseComObject(worksheet);
+            Marshal.FinalReleaseComObject(workbook);
+            Marshal.FinalReleaseComObject(myExcel);
+
+
+            return val;
+
+
+        }
+
+
 
         public static string getExcelSheetNames(string pathToExcelFile)
         {
